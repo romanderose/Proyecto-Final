e65609f010a83dcf8c6253675c1c2a9213c1220d
romanderose <romandealdobonzi@gmail.com>
2023-11-16 18:37:57 -0300

    trabajo terminado

--- juego.js
+++ juego.js
@@ -1,43 +1,143 @@
-let img1 = document.getElementsByTagName("img")[1]
+let imagen = document.querySelectorAll(".imagenesjuego")
 
-/*
-let img2 = document.getElementsByTagName("img")[2]
-let img3 = document.getElementsByTagName("img")[3]
-let imagenes = [img1, img2, img3]
-*/
+//console.log(imagen)
 
+let destino = document.querySelectorAll(".zonadestino")
+let botonreinicio= document.querySelector(".reinicio")
 
-let destino1 = document.querySelectorAll("div")[1]
+//console.dir(botonreinicio)
 
-/*
-let destino2 = document.querySelectorAll("div")[2]
-let destino3 = document.querySelectorAll("div")[3]
-let destinos = [destino1, destino2, destino3]
-*/
+function MouseOverAndLeave(a,b,c){
+    /*Para que al pasar el mouse por encima de las zonas destino se pongan en
+    negro y cuando salgo de ellas vuelvan a gris*/
 
+    //zona de la izquierda
+    a.addEventListener('mouseover', ()=>{
+        a.style.backgroundColor="black";
+    })
+    a.addEventListener('mouseout',()=>{
+        a.style.backgroundColor="grey";
+    })
 
-//imagenes a trasladar
+    //zona del centro
+    b.addEventListener('mouseover', ()=>{
+        b.style.backgroundColor="black";
+    })
+    b.addEventListener('mouseout',()=>{
+        b.style.backgroundColor="grey";
+    })
 
-imagenes.addEventListener('dragstart', iniciotraslado);
-imagenes.addEventListener('dragend', finaltraslado);
-
-
-function iniciotraslado(evento){
-    evento.dataTransfer.setData('Text',"./Rompe-1.png")
+    //zona de la izquierda
+    c.addEventListener('mouseover', ()=>{
+        c.style.backgroundColor="black";
+    })
+    c.addEventListener('mouseout',()=>{
+        c.style.backgroundColor="grey";
+    })
 }
 
-function finaltraslado(evento){
-    alert("Proceso finalizado")
+MouseOverAndLeave(destino[0],destino[1],destino[2])
+
+//--------------------------------------------------------------------------------------------
+
+/*DATOS DE LA IMAGEN DE LA IZQUIERDA (QUE AL ARMAR CORRECTAMENTE EL 
+ROMPECABEZAS QUEDARÍA EN EL CENTRO)*/
+
+//eventos de la imagen a arrastrar
+
+imagen[0].addEventListener('dragstart', inicioTraslado);
+
+imagen[0].addEventListener('dragend', finalTraslado);
+
+function inicioTraslado(evento){
+    evento.dataTransfer.setData('Text',"./rompe2.png")
+}
+
+function finalTraslado(evento){
+    imagen[0].style.visibility="hidden";
 }
 
 
-//destino
+//eventos de la zona de destino
 
-destino1.addEventListener('drop', soltarelemento);
+destino[1].addEventListener('dragover', prevenirDefault);
 
-function soltarelemento(evento){
+destino[1].addEventListener('drop', soltarElemento);
 
-    let data = evento.dataTransfer.getdata('Text');
-    destino1.innerHTML= '<img id="img1">'
+function soltarElemento(evento){
+    let dataimagen = evento.dataTransfer.getData('Text')
+
+    destino[1].innerHTML= `<img id="central" src="${dataimagen}"/>`
 }
 
+function prevenirDefault(evento){
+    evento.preventDefault()
+}
+
+//--------------------------------------------------------------------------------------------
+
+/*DATOS DE LA IMAGEN DEL CENTRO (QUE AL ARMAR CORRECTAMENTE EL 
+ROMPECABEZAS QUEDARÍA EN LA IZQUIERDA)*/
+
+//eventos de la imagen a arrastrar
+
+imagen[1].addEventListener('dragstart', inicioTraslado1);
+
+imagen[1].addEventListener('dragend', finalTraslado1);
+
+function inicioTraslado1(evento){
+    evento.dataTransfer.setData('Text',"./rompe1.png")
+}
+
+function finalTraslado1(evento){
+    imagen[1].style.visibility="hidden";
+}
+
+
+//eventos de la zona de destino
+
+destino[0].addEventListener('dragover', prevenirDefault);
+
+destino[0].addEventListener('drop', soltarElemento1);
+
+function soltarElemento1(evento){
+    let dataimagen1 = evento.dataTransfer.getData('Text')
+
+    destino[0].innerHTML= `<img id="central" src="${dataimagen1}"/>`
+}
+
+//--------------------------------------------------------------------------------------------
+
+/*DATOS DE LA IMAGEN DE LA DERECHA (QUE AL ARMAR CORRECTAMENTE EL 
+ROMPECABEZAS QUEDARÍA TAMBIÉN EN LA DERECHA)*/
+
+//eventos de la imagen a arrastrar
+
+imagen[2].addEventListener('dragstart', inicioTraslado2);
+
+imagen[2].addEventListener('dragend', finalTraslado2);
+
+function inicioTraslado2(evento){
+    evento.dataTransfer.setData('Text',"./rompe3.png")
+}
+
+function finalTraslado2(evento){
+    imagen[2].style.visibility="hidden";
+}
+
+
+//eventos de la zona de destino
+
+destino[2].addEventListener('dragover', prevenirDefault);
+
+destino[2].addEventListener('drop', soltarElemento2);
+
+function soltarElemento2(evento){
+    let dataimagen2 = evento.dataTransfer.getData('Text')
+
+    destino[2].innerHTML= `<img id="central" src="${dataimagen2}"/>`
+}
+
+//--------------------------------------------------------------------------------------------
+
+botonreinicio.style.marginTop="-50px"
